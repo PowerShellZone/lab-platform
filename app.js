aa3383aa5ecc683f5784c0480c279c3f765b0015
Ubuntu <azureuser@lab-gateway.kgxm0baplxsunhuy3kr4kfneff.wawx.internal.cloudapp.net>
2026-03-12 18:43:17 +0000

    initial lab platform

---
+++ app.js
@@ -0,0 +1,22 @@
+https://powershell-lab-function-edhwegapcge0ejf6.westeurope-01.azurewebsites.net/api/startlabasync function startLab() {
+
+document.getElementById("status").innerText = "Starting lab...";
+
+const response = await fetch(
+"https://powershell-lab-function-edhwegapcge0ejf6.westeurope-01.azurewebsites.net/api/startlab",
+{
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+lab: "ad-lab",
+user: "demo"
+})
+});
+
+const data = await response.json();
+
+document.getElementById("status").innerText = data.message;
+
+}
